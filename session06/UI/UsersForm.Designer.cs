58da38d4d9d8284a0b43f1f6215b704aa028f8bc
Pashaie <pashaie@gmail.com>
2025-01-27 20:44:52 +0330

    pagination

--- session06/UI/UsersForm.Designer.cs
+++ session06/UI/UsersForm.Designer.cs
@@ -29,44 +29,124 @@ partial class UsersForm
     private void InitializeComponent()
     {
         groupBoxUsers = new GroupBox();
+        buttonSave = new Button();
         dataGridViewUsers = new DataGridView();
+        buttonLast = new Button();
+        buttonNext = new Button();
+        buttonPrev = new Button();
+        buttonFirst = new Button();
+        labelPage = new Label();
+        groupBoxUsers.SuspendLayout();
         ((System.ComponentModel.ISupportInitialize)dataGridViewUsers).BeginInit();
         SuspendLayout();
         // 
         // groupBoxUsers
         // 
+        groupBoxUsers.Controls.Add(buttonSave);
         groupBoxUsers.Location = new Point(12, 12);
         groupBoxUsers.Name = "groupBoxUsers";
-        groupBoxUsers.Size = new Size(742, 249);
+        groupBoxUsers.Size = new Size(965, 291);
         groupBoxUsers.TabIndex = 0;
         groupBoxUsers.TabStop = false;
         groupBoxUsers.Text = "groupBoxUsers";
         groupBoxUsers.Enter += groupBoxUsers_Enter;
         // 
+        // buttonSave
+        // 
+        buttonSave.Location = new Point(884, 262);
+        buttonSave.Name = "buttonSave";
+        buttonSave.Size = new Size(75, 23);
+        buttonSave.TabIndex = 0;
+        buttonSave.Text = "Save";
+        buttonSave.UseVisualStyleBackColor = true;
+        buttonSave.Click += buttonSave_Click;
+        // 
         // dataGridViewUsers
         // 
         dataGridViewUsers.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-        dataGridViewUsers.Location = new Point(12, 267);
+        dataGridViewUsers.Location = new Point(12, 352);
         dataGridViewUsers.Name = "dataGridViewUsers";
-        dataGridViewUsers.Size = new Size(742, 275);
+        dataGridViewUsers.Size = new Size(965, 256);
         dataGridViewUsers.TabIndex = 1;
+        // 
+        // buttonLast
+        // 
+        buttonLast.Location = new Point(12, 625);
+        buttonLast.Name = "buttonLast";
+        buttonLast.Size = new Size(90, 33);
+        buttonLast.TabIndex = 2;
+        buttonLast.Text = "<<";
+        buttonLast.UseVisualStyleBackColor = true;
+        buttonLast.Click += buttonLast_Click;
+        // 
+        // buttonNext
+        // 
+        buttonNext.Location = new Point(108, 625);
+        buttonNext.Name = "buttonNext";
+        buttonNext.Size = new Size(90, 33);
+        buttonNext.TabIndex = 3;
+        buttonNext.Text = "<";
+        buttonNext.UseVisualStyleBackColor = true;
+        buttonNext.Click += buttonNext_Click;
+        // 
+        // buttonPrev
+        // 
+        buttonPrev.Location = new Point(288, 625);
+        buttonPrev.Name = "buttonPrev";
+        buttonPrev.Size = new Size(90, 33);
+        buttonPrev.TabIndex = 4;
+        buttonPrev.Text = ">";
+        buttonPrev.UseVisualStyleBackColor = true;
+        buttonPrev.Click += buttonPrev_Click;
+        // 
+        // buttonFirst
+        // 
+        buttonFirst.Location = new Point(384, 625);
+        buttonFirst.Name = "buttonFirst";
+        buttonFirst.Size = new Size(90, 33);
+        buttonFirst.TabIndex = 5;
+        buttonFirst.Text = ">>";
+        buttonFirst.UseVisualStyleBackColor = true;
+        buttonFirst.Click += buttonFirst_Click;
+        // 
+        // labelPage
+        // 
+        labelPage.AutoSize = true;
+        labelPage.Location = new Point(229, 634);
+        labelPage.Name = "labelPage";
+        labelPage.Size = new Size(30, 15);
+        labelPage.TabIndex = 6;
+        labelPage.Text = "1/10";
         // 
         // UsersForm
         // 
         AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(766, 554);
+        ClientSize = new Size(989, 670);
+        Controls.Add(labelPage);
+        Controls.Add(buttonFirst);
+        Controls.Add(buttonPrev);
+        Controls.Add(buttonNext);
+        Controls.Add(buttonLast);
         Controls.Add(dataGridViewUsers);
         Controls.Add(groupBoxUsers);
         Name = "UsersForm";
         Text = "UsersForm";
         Load += UsersForm_Load;
+        groupBoxUsers.ResumeLayout(false);
         ((System.ComponentModel.ISupportInitialize)dataGridViewUsers).EndInit();
         ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
 
     private GroupBox groupBoxUsers;
     private DataGridView dataGridViewUsers;
+    private Button buttonSave;
+    private Button buttonLast;
+    private Button buttonNext;
+    private Button buttonPrev;
+    private Button buttonFirst;
+    private Label labelPage;
 }
--- session06/UI/UsersForm.cs
+++ session06/UI/UsersForm.cs
@@ -13,6 +13,10 @@ using System.Windows.Forms;
 namespace session06.UI;
 public partial class UsersForm : Form
 {
+    int currentPage = 1;
+    int totalPages = 1;
+    int pageSize = 3;
+
     public UsersForm()
     {
         InitializeComponent();
@@ -21,10 +25,71 @@ public partial class UsersForm : Form
     private void UsersForm_Load(object sender, EventArgs e)
     {
         groupBoxUsers.DesignForm(typeof(User));
+        loadData();
     }
 
     private void groupBoxUsers_Enter(object sender, EventArgs e)
     {
 
+    }
+
+    private void buttonSave_Click(object sender, EventArgs e)
+    {
+        //Genrics
+        //Unboxig
+        var model = (User)groupBoxUsers.GetFormData(typeof(User));
+        var ctx = new AppDbContext();
+        ctx.Add(model);
+        ctx.SaveChanges();
+        loadData();
+        groupBoxUsers.SetFormData(new User());
+    }
+
+    private void loadData()
+    {
+        var ctx = new AppDbContext();
+        //Skip(10) -> 10 avalo ignore
+        //Take(3) -> 3 ta ro begir
+        //page 1 = skip(0 = pageSize * page - 1).take(pageSize)
+        //page 2 = skip(3 = pageSize * page - 1).take(pageSize)
+        //page 3 = skip(6 = pageSize * page - 1).take(pageSize)
+        dataGridViewUsers.DataSource = ctx.Users
+            .Skip(pageSize * (currentPage - 1))
+            .Take(pageSize).ToList();
+
+        int total = ctx.Users.Count();
+        totalPages = Convert.ToInt32(Math.Ceiling(total / Convert.ToDecimal(pageSize)));
+
+        labelPage.Text = $"{currentPage}/{totalPages}";
+    }
+
+    private void buttonFirst_Click(object sender, EventArgs e)
+    {
+        currentPage = 1;
+        loadData();
+    }
+
+    private void buttonPrev_Click(object sender, EventArgs e)
+    {
+        if (currentPage > 1)
+        {
+            currentPage -= 1;
+            loadData();
+        }
+    }
+
+    private void buttonNext_Click(object sender, EventArgs e)
+    {
+        if(currentPage < totalPages)
+        {
+            currentPage += 1;
+            loadData();
+        }
+    }
+
+    private void buttonLast_Click(object sender, EventArgs e)
+    {
+        currentPage = totalPages;
+        loadData();
     }
 }
