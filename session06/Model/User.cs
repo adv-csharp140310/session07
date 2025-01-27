d1fe47035ca0689872850b45f9b80737f98e4563
Pashaie <pashaie@gmail.com>
2025-01-27 18:41:53 +0330

    Data Annotation (attributes)

--- session06/Model/User.cs
+++ session06/Model/User.cs
@@ -1,10 +1,18 @@
-﻿namespace session06.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace session06.Model;
 
 //POCO - Plain Old CLR Object
 //Entity
+
+//Data Annotation (attributes)
+//Fluent API
 public class User
 {
     public int Id { get; set; }
+
+    [MaxLength(100)]
+    [Required]
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
